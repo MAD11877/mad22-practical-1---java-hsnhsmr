a20058b2e612bc08327c7d2074bb836b4f42f5b3
hsnhsmr <103978565+hsnhsmr@users.noreply.github.com>
2022-04-19 20:49:29 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -2,9 +2,10 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    javac Question1.java;
-    java Question1;
-    System.out.println("Hello World!");
-     
+    /**
+     * Print "Hello World!"
+     * 
+     * Hint: System.out.println(...)
+     */
   }
 }
